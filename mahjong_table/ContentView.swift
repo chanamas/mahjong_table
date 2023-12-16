c98279873d220cbc16064e744208557a8eecd3db
Chanamas <hiro1101stkt@gmail.com>
2023-12-16 21:11:05 +0900

    Initial Commit

---
+++ mahjong_table/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mahjong_table
+//
+//  Created by Hiroto Amada on 2023/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mahjong_table/mahjong_tableApp.swift
@@ -0,0 +1,17 @@
+//
+//  mahjong_tableApp.swift
+//  mahjong_table
+//
+//  Created by Hiroto Amada on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct mahjong_tableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
